9e5bd8f538b8dfc3441f17c7354edcbbcb522f54
Romsesito <graphicsrom@gmail.com>
2024-07-07 23:04:41 -0500

    Commit Previo a la bd

---
+++ UIBlackjack/Models/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIBlackjack.Models
+{
+    public class UserData
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string fono { get; set; }
+
+    }
+}
--- UIBlackjack/ViewModels/InfoViewModel.cs
+++ UIBlackjack/ViewModels/InfoViewModel.cs
@@ -1,16 +1,31 @@
 ﻿// ViewModels/InfoViewModel.cs
 using System.Windows.Input;
 using Microsoft.Maui.Controls;
+using UIBlackjack.Models;
 
 namespace UIBlackjack.ViewModels
 {
-    public class InfoViewModel
+    public class InfoViewModel : BindableObject
+
     {
+        private UserData _userData;
+        public UserData UserData
+        {
+            get => _userData;
+            set
+            {
+                _userData = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SaveCommand { get; }
         public ICommand NavigateToPageCommand { get; }
 
         public InfoViewModel()
         {
             NavigateToPageCommand = new Command(async () => await NavigateToPage("Menu"));
+            SaveCommand = new Command(async () => await SaveData());
         }
 
         private async Task NavigateToPage(string pagename)
@@ -18,6 +33,17 @@ namespace UIBlackjack.ViewModels
             var navigation = Application.Current.MainPage.Navigation;
             await navigation.PushAsync(new Views.Menu());
 
+        }
+
+
+  
+
+        private async Task SaveData()
+        {
+            // Aquí puedes agregar la lógica para guardar los datos
+
+            // Mostrar mensaje de guardado exitoso
+            await Application.Current.MainPage.DisplayAlert("Datos Guardados", "Guardado exitosamente", "OK");
         }
     }
 }
